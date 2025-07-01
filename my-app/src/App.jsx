16f8276d326fac991b895dc2e5335cfdf0dbad7a
RAKESH-KUMAR-N3 <nrakeshkumar36@gmail.com>
2025-07-02 00:21:23 +0530

    this is new commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Todolist from './todolist/Todolist';
+
+
+
+function App() {
+  return (
+    <>
+     <Todolist />
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/todolist/Todolist.jsx
@@ -0,0 +1,19 @@
+import './Todolist.css';
+
+
+function Todolist() {
+  return (
+    <div className='todo-container'> 
+      <h1 className='todo-heading'>Todolist</h1>
+      <p className='todo-paragraph'>Each day I will accomplish one thing on my todolist</p>
+      <h1 className='todo-subheading'>Read a Book</h1>
+      <p className='todo-paragraph2'>I don't think that the hiuman race will survive the 
+        next thoyusand years, unless we spread into space 
+        there are too many accidents that can befall life on 
+        a single planet. But i'm an optimis        htdtyrfugfffyyfjyftiufy</p>
+      <button className='todo-button'>Start</button>
+      <p classNme='last-paragraph'>Don't skip introduction</p>
+  </div>)
+}
+
+export default Todolist;
